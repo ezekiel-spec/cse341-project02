f19b007ef96a67f5a19c55305b4165d82ebcc35c
ezekiel-spec <your-email@example.com>
2026-04-01 08:53:31 +0100

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -6,15 +6,26 @@ router.use('/', require('./swagger-router'));
 router.use('/movies', require('./movies'));
 router.use('/actors', require('./actors'));
 
-// Login Route: This redirects the user to GitHub
+// 1. Login Route: This redirects the user to GitHub
 router.get('/login', passport.authenticate('github', (req, res) => {}));
 
-// Logout Route: This clears the session
+// 2. Logout Route: This clears the session
 router.get('/logout', function(req, res, next) {
     req.logout(function(err) {
         if (err) { return next(err); }
         res.redirect('/');
     });
+});
+
+// 3. Callback Route: GitHub sends the user back here with a 'code'
+// This is the part that was missing and causing the error!
+router.get('/github/callback', passport.authenticate('github', {
+    failureRedirect: '/api-docs', 
+    session: false // We handle the session manually below
+}), (req, res) => {
+    // This line is CRITICAL: it saves the user data into your session
+    req.session.user = req.user; 
+    res.redirect('/');
 });
 
 module.exports = router;
